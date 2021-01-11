3aa131d4f169e860895fcc81144f01c06c89e611
Gleb Varganov <webgleb@gmail.com>
2021-01-12 03:23:31 +0500

    Add html design template

---
+++ resources/views/_sidebar-links.blade.php
@@ -0,0 +1,10 @@
+<ul>
+    <li class="font-bold text-lg mb-4 block" href="/">Home</li>
+    <li class="font-bold text-lg mb-4 block" href="/explore">Explore</li>
+    <li class="font-bold text-lg mb-4 block" href="#">Notifications</li>
+    <li class="font-bold text-lg mb-4 block" href="#">Messages</li>
+    <li class="font-bold text-lg mb-4 block" href="#">Bookmarks</li>
+    <li class="font-bold text-lg mb-4 block" href="#">Lists</li>
+    <li class="font-bold text-lg mb-4 block" href="#">Profile</li>
+    <li class="font-bold text-lg mb-4 block" href="#">More</li>
+</ul>
