c6c5ed4557f91cd9e93641dd8dca450169f15734
Gleb Varganov <webgleb@gmail.com>
2021-01-12 21:23:07 +0500

    Dynamic feed

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,4 +40,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getAvatarAttribute($size="50")
+    {
+        return "https://i.pravatar.cc/" . $size . "?u=" . $this->email;
+    }
+
+    public function timeline()
+    {
+        return Tweet::where('user_id', $this->id)->latest()->get();
+    }
 }
--- resources/views/_tweet.blade.php
+++ resources/views/_tweet.blade.php
@@ -1,12 +1,10 @@
 <div class="flex p-4 border-b border-b-gray-400">
     <div class="mr-2 flex-shrink-0">
-        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2" />
+        <img src="{{ auth()->user()->getAvatarAttribute(40) }}" alt="" class="rounded-full mr-2" />
     </div>
     <div>
-        <h5 class="font-bold mb-4">John Doe</h5>
+        <h5 class="font-bold mb-4">{{ $tweet->user->name }}</h5>
 
-        <p class="text-sm">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.
-        </p>
+        <p class="text-sm">{{ $tweet->body }}</p>
     </div>
 </div>
