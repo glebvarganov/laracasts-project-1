cc54f6812a448235a388ee5ce8ed29a46b040135
Gleb Varganov <webgleb@gmail.com>
2021-02-17 01:44:42 +0500

    Add JS code to count tweet symbols

--- resources/views/tweets/index.blade.php
+++ resources/views/tweets/index.blade.php
@@ -4,4 +4,29 @@
 
         @include('_timeline')
     </div>
+
+    @section('page-scripts')
+        <script>
+            const publishPanel = document.querySelector('#publish-panel');
+            const symbolsCounter = document.querySelector('#publish-symbols-count');
+
+            function publishPanelManipulation() {
+                symbolsCounter.innerHTML = this.value.length;
+                if (this.value.length > 255) {
+                    symbolsCounter.classList.add('text-red-500');
+                } else {
+                    symbolsCounter.classList.remove('text-red-500');
+                }
+            };
+
+            publishPanel.addEventListener('input', publishPanelManipulation);
+
+            document.addEventListener('DOMContentLoaded', function () {
+                symbolsCounter.innerHTML = publishPanel.value.length;
+            });
+        </script>
+    @endsection
+
 </x-app>
+
+
